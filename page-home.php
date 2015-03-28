cb3d1a4f303cbccaa2d12418a103fc493b3ea047
Elliott-Greaves <greaveselliott@me.com>
2015-03-28 00:48:37 +0000

    Midnight changes - might be bollocks

--- page-home.php
+++ page-home.php
@@ -6,30 +6,31 @@
 
 <?php get_header(); ?>
 <div class="full-screen background-blue opacity-30">
+    <div class="position-relative width-100 height-100">
+        <section class="starting-point background-blue position-absolute bottom-0 width-100">
+            <div class="container">
+                <div class="row">
+                    <div class="row-sm-12">
+                        <h2>Choose your starting point</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="row-sm-3 background-orange">
+                        <p>Discover how to curb excessive spending, apportion costs, and the best tariffs to suit your needs.</p>
+                    </div>
+                    <div class="row-sm-3">
 
-    <section class="starting-point background-blue">
-        <div class="container">
-            <div class="row">
-                <div class="row-sm-12">
-                    <h2>Choose your starting point</h2>
+                    </div>
+                    <div class="row-sm-3">
+
+                    </div>
+                    <div class="row-sm-3">
+
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="row-sm-3 background-orange">
-                    <p>Discover how to curb excessive spending, apportion costs, and the best tariffs to suit your needs.</p>
-                </div>
-                <div class="row-sm-3">
-
-                </div>
-                <div class="row-sm-3">
-
-                </div>
-                <div class="row-sm-3">
-
-                </div>
-            </div>
-        </div>
-    </section>
+        </section>
+    </div>
 </div>
 
 
