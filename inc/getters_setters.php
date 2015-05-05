0b3ff60c3085c59e87dc76ed8994877234720447
Elliott-Greaves <greaveselliott@me.com>
2015-05-05 18:57:10 +0100

    Modified get_acf_image_url() function: now accepts post_id as a parameter

--- inc/getters_setters.php
+++ inc/getters_setters.php
@@ -162,14 +162,14 @@ function eemjii_post_command ($posts, $template_file) {
 function eemjii_get_featured_image_url ($post_ID, $size = 'large', $attr = array()) {
     return $featured_image = has_post_thumbnail($post_ID) ? // CONDITION: This post thumbnail has a featured image?
         get_the_post_thumbnail($post_ID, $size,$attr) :          // TRUE: save the post thumnail
-            '<img src="http://placehold.it/300x180" class="width-100 height-100">';                  // FALSE: use the default image
+            '<img src="http://placehold.it/100x100" class="width-100 height-100">';                  // FALSE: use the default image
 }
 
-function eemjii_get_acf_image_url ($field_name, $sub_field = false) {
+function eemjii_get_acf_image_url ($field_name, $sub_field = false, $post_id = null) {
     if (!$sub_field):
-        $image = get_field($field_name);
+        $image = get_field($field_name, $post_id);
         elseif($sub_field):
-            $image = get_sub_field($field_name);
+            $image = get_sub_field($field_name, $post_id);
             endif;
     return !empty($image) ?                 // CONDITION: Is this custom field image empty?
         $image :                            // TRUE: Return the image URL
