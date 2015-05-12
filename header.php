d92db1fe700d88b4390611ffb39c8e58dae15410
Elliott-Greaves <greaveselliott@me.com>
2015-05-12 14:44:19 +0100

    Elliot Tume added the predicted response tracking code and added wp_title(); to the <title> tag

--- header.php
+++ header.php
@@ -1,40 +1,20 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-    <title><?php
-        /*
-         * Print the <title> tag based on what is being viewed.
-         */
-        global $page, $paged;
-
-        wp_title( '|', true, 'right' );
-
-        // Add the blog name.
-        bloginfo( 'name' );
-
-        // Add the blog description for the home/front page.
-        $site_description = get_bloginfo( 'description', 'display' );
-        if ( $site_description && ( is_home() || is_front_page() ) )
-            echo " | $site_description";
-
-        // Add a page number if necessary:
-        if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() )
-            echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
-
-        ?></title>
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title><?php wp_title(''); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
     <script type="text/javascript" src="http://aws.predictiveresponse.net/http/trac.js"> </script>
     <script type="text/javascript"> trackPredictive(); </script>
 </head>
@@ -45,15 +25,18 @@
     specifically on the services template
 -->
 <body <?php body_class( the_slug(false)); ?>>
+<?php /*Added by Elliot Tume start*/ ?>
+<?php if ( function_exists( 'gtm4wp_the_gtm_tag' ) ) { gtm4wp_the_gtm_tag(); } ?>
+<?php /*Added by Elliot Tume begin*/ ?>
 <div class="social-sidebar display-none-important-tablet-portrait">
     <ul>
         <li class="twitter"><a href="https://twitter.com/IPSolutionsUK" target="_blank" title="Follow IP Solutions on twitter" class="text-white"><i class="fa fa-twitter"></i></a></li>
-        <li class="linkedin"><a href="https://www.linkedin.com/company/ip-solutions-uk-ltd" target="_blank" title="Connect with IP Solutions on Linked-in" class="text-white"><i class="fa fa-facebook"></i></a></li>
+        <li class="linkedin"><a href="https://www.linkedin.com/company/ip-solutions-uk-ltd" target="_blank" title="Connect with IP Solutions on Linked-in" class="text-white"><i class="fa fa-linkedin"></i></a></li>
         <li class="facebook"><a href="https://www.facebook.com/ipsolutionsuk" target="_blank" title="Friend us on Facebook!" class="text-white"><i class="fa fa-facebook"></i></a></li>
         <li class="email"><a href="mailto:info@ipsolutions.co.uk" class="text-white"><i class="fa fa-envelope"></i></a></li>
     </ul>
 </div>
-    <div class="featured-image-container position-relative">
+<div class="featured-image-container position-relative">
     <!-- The .featured-image-container's closing tag exists in the page template file for example page-home.php -->
     <div id="header-main-container" class="width-100">
         <header id="header-main" class="background-white opacity-80">
@@ -81,9 +64,9 @@
                         </div>
                         <!-- Navigation -->
                         <div class="row height-50">
-                             <div class="col-sm-12 padding-right-0 padding-0-tablet-portrait height-100-important">
-                                    <h2 class="hidden"><?php echo bloginfo('name'); ?> | Website Navigation</h2>
-                                    <?php ubermenu( 'main' , array( 'theme_location' => 'primary' ) ); ?>
+                            <div class="col-sm-12 padding-right-0 padding-0-tablet-portrait height-100-important">
+                                <h2 class="hidden"><?php echo bloginfo('name'); ?> | Website Navigation</h2>
+                                <?php ubermenu( 'main' , array( 'theme_location' => 'primary' ) ); ?>
                             </div>
                         </div>
                     </div>
